0fbcec4028c0a5bae73d4f25ed48411fa40f436d
jpfl15 <50215860+jpfl15@users.noreply.github.com>
2021-10-21 23:46:19 -0700

    Add files via upload

---
+++ PruebaGCS/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaGCS
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "HELLO WORLD";
+            label2.Text = DateTime.Now.ToLongTimeString() + " " + DateTime.Now.ToLongDateString();
+            //label2.Location = new Point(this.Width / 2 - label2.Width / 2, this.Height - label2.Height / 2);
+        }
+
+        
+    }
+}
